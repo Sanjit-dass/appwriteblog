f90188bdf0a1840370659cf078fd62bdbf3c1b84
sanjit Das <sanjit9842@gmail.com>
2025-08-01 23:19:11 +0530

    Fix responsiveness in AllPosts page

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,28 +1,34 @@
-import React, {useState, useEffect} from 'react'
-import { Container, PostCard } from '../components'
+import React, { useState, useEffect } from 'react';
+import { Container, PostCard } from '../components';
 import appwriteService from "../appwrite/config";
 
 function AllPosts() {
-    const [posts, setPosts] = useState([])
-    useEffect(() => {}, [])
-    appwriteService.getPosts([]).then((posts) => {
-        if (posts) {
-            setPosts(posts.documents)
-        }
-    })
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    appwriteService.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
+
   return (
-    <div className='w-full py-8'>
-        <Container>
-            <div className='flex flex-wrap'>
-                {posts.map((post) => (
-                    <div key={post.$id} className='p-2 w-1/4'>
-                        <PostCard {...post} />
-                    </div>
-                ))}
+    <div className='w-full py-8 dark:bg-gradient-to-r dark:from-slate-900 dark:via-gray-800 dark:to-black'>
+      <Container>
+        <div className='flex flex-wrap -mx-2'>
+          {posts.map((post) => (
+            <div
+              key={post.$id}
+              className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2'
+            >
+              <PostCard {...post} />
             </div>
-            </Container>
+          ))}
+        </div>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default AllPosts
+export default AllPosts;
